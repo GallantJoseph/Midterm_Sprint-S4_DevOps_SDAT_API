90f5d99f569572ab3e3c3fb948ef9c34d1c299ba
GallantJoseph <gallant.joseph@outlook.com>
2025-10-21 18:47:52 -0230

    City Get by id, Put, Delete

--- src/main/java/rest/city/CityController.java
+++ src/main/java/rest/city/CityController.java
@@ -1,6 +1,7 @@
 package rest.city;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +22,23 @@ public class CityController {
         return cityService.createCity(city);
     }
 
+    @GetMapping("/{id}")
+    public City getCityById(@PathVariable long id) {
+        return cityService.getCityById(id);
+    }
+
     @GetMapping
     public List<City> getAllCities(){
         return cityService.getAllCities();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<City> updateCity(@PathVariable long id, @RequestBody City city) {
+        return ResponseEntity.ok(cityService.updateCity(id, city));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCity(@PathVariable long id) {
+        cityService.deleteCity(id);
     }
 }
--- src/main/java/rest/city/CityService.java
+++ src/main/java/rest/city/CityService.java
@@ -14,7 +14,26 @@ public class CityService {
         return cityRepository.save(city);
     }
 
+    public City getCityById(long id) {
+        return cityRepository.findById(id).orElse(null);
+    }
+
     public List<City> getAllCities(){
         return (List<City>) cityRepository.findAll();
+    }
+
+    public City updateCity(long id, City updatedCity) {
+        return cityRepository.findById(id)
+                .map(city -> {
+                    city.setName(updatedCity.getName());
+                    city.setProvince(updatedCity.getProvince());
+                    city.setPopulation(updatedCity.getPopulation());
+                    return cityRepository.save(city);
+                })
+                .orElse(null);
+    }
+
+    public void deleteCity(long id) {
+        cityRepository.deleteById(id);
     }
 }
