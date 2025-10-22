4f78551c036645800072007d98d528d537a7cc7d
TheLostMrJ <greenslade_12@hotmail.com>
2025-10-22 11:16:54 -0230

    Aircraft package files updated(Aircraft)/created (controller, service, repository)
    - Tweaked Aircraft.java to initialize passengers and airports as new ArrayList<> so they return [] instead of null
    
    - Added toString in Aircraft.java
    
    - Cleaned up Aircraft.java formatting
    
    - Made AircraftController.java with endpoints for CRUD  and Question 3
    
    -Created AircraftRepository.java for database stuff using CrudRepository
    
    - Tested endpoints with injected data from other classes to confirm everything in working order

--- src/main/java/rest/aircraft/AircraftController.java
+++ src/main/java/rest/aircraft/AircraftController.java
@@ -1,15 +1,53 @@
 package rest.aircraft;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import rest.airport.Airport;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/aircraft")
+@RequestMapping({"/aircraft"})
 @CrossOrigin
 public class AircraftController {
-    
     @Autowired
     private AircraftService aircraftService;
+
+    @GetMapping({""})
+    public ResponseEntity<Iterable<Aircraft>> getAllAircraft(){
+        return ResponseEntity.ok(aircraftService.getAllAircraft());
+    }
+
+    @GetMapping({"/{id}"})
+        public ResponseEntity<Aircraft> getAircraftById(@PathVariable Long id){
+        return ResponseEntity.ok(aircraftService.getAircraftById(id));
+        }
+
+    @PostMapping({""})
+    public ResponseEntity<Aircraft> createAircraft(@RequestBody Aircraft aircraft){
+        return ResponseEntity.ok(aircraftService.createAircraft(aircraft));
+    }
+
+    @PutMapping({"/{id}"})
+    public ResponseEntity<Aircraft> updateAircraft(@PathVariable Long id, @RequestBody Aircraft aircraft){
+        return ResponseEntity.ok(aircraftService.updateAircraft(id, aircraft));
+    }
+    @DeleteMapping({"/{id}"})
+    public void deleteAircraftById(@PathVariable Long id){
+        aircraftService.deleteAircraftById(id);
+    }
+
+    // TODO ********** REVIEW WITH TEAM TO FIX *********
+
+    @GetMapping({"/{id}/airports"})
+    public ResponseEntity<List<Airport>> getAirportsByAircraft(@PathVariable Long id){
+        return ResponseEntity.ok(aircraftService.getAirportsByAircraft(id));
+    }
+
+    // we need this for Question 3
+    @PostMapping({"/{id}/airports/{airportId}"})
+    public void addAirportToAircraft(@PathVariable Long id, @PathVariable Long airportId){
+        aircraftService.addAirportToAircraft(id, airportId);
+    }
 }
--- src/main/java/rest/aircraft/AircraftService.java
+++ src/main/java/rest/aircraft/AircraftService.java
@@ -2,9 +2,78 @@ package rest.aircraft;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rest.airport.Airport;
+import rest.airport.AirportRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AircraftService {
     @Autowired
-    private AircraftController aircraftController;
+    private AircraftRepository aircraftRepository;
+
+    @Autowired
+    private  AirportRepository airportRepository;
+
+  // added to match teachers format of casting.
+    public Iterable<Aircraft> getAllAircraft() {
+        return  aircraftRepository.findAll();
+    }
+
+    public Aircraft getAircraftById(Long id){
+        Optional<Aircraft> aircraftOptional = aircraftRepository.findById(id);
+        return aircraftOptional.orElse(null);
+    }
+
+    public Aircraft createAircraft(Aircraft newAircraft){
+        return aircraftRepository.save(newAircraft);
+    }
+
+    // Can change this if we establish new arrayList<> in class files instead of leaving it null.
+    public Aircraft updateAircraft(Long id, Aircraft updatedAircraft){
+        Optional<Aircraft> aircraftOptional = aircraftRepository.findById(id);
+        if (aircraftOptional.isPresent()){
+            Aircraft aircraftToUpdate = aircraftOptional.get();
+            aircraftToUpdate.setType(updatedAircraft.getType());
+            aircraftToUpdate.setAirlineName(updatedAircraft.getAirlineName());
+            aircraftToUpdate.setNumberOfPassengers(updatedAircraft.getNumberOfPassengers());
+            return aircraftRepository.save(aircraftToUpdate);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteAircraftById(Long id){
+        aircraftRepository.deleteById(id);
+    }
+
+    public List<Airport> getAirportsByAircraft(Long id){
+        Optional<Aircraft> aircraftOptional = aircraftRepository.findById(id);
+        if (aircraftOptional.isPresent()){
+            return aircraftOptional.get().getAirports();
+        } else {
+            return null;
+        }
+    }
+
+
+    // can also be fixed if we esablish the new arrayList<> in class files
+    public void addAirportToAircraft(Long aircraftId, Long airportId){
+        Optional<Aircraft> aircraftOptional = aircraftRepository.findById(aircraftId);
+        Optional<Airport> airportOptional = airportRepository.findById(airportId);
+        if (aircraftOptional.isPresent() && airportOptional.isPresent()) {
+            Aircraft aircraft = aircraftOptional.get();
+            Airport airport = airportOptional.get();
+            if (airport.getAircraftList() == null) {
+                airport.setAircraftList(new ArrayList<>());
+            }
+            aircraft.getAirports().add(airport);
+            airport.getAircraftList().add(aircraft);
+            aircraftRepository.save(aircraft);
+            airportRepository.save(airport);
+        }
+    }
 }
+
