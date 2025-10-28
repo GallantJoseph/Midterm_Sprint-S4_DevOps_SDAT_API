b9241a2e1f24d34e8571968f03a6f0c4dbae7e31
TheLostMrJ <greenslade_12@hotmail.com>
2025-10-28 19:04:01 -0230

    Updated Contoller, Repository, Service
    Fixed relationships for aircraft, and cleaned up CRUD overal.

--- src/main/java/rest/aircraft/AircraftService.java
+++ src/main/java/rest/aircraft/AircraftService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import rest.airport.Airport;
 import rest.airport.AirportRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,31 +16,32 @@ public class AircraftService {
     @Autowired
     private AirportRepository airportRepository;
 
+    public Iterable<Aircraft> getAllAircraftByPassengerId (Long passengerId) {
+        return aircraftRepository.findAllByPassengers_Id(passengerId);
+    }
+
     public Iterable<Aircraft> getAllAircraft() {
         return  aircraftRepository.findAll();
     }
 
     public Aircraft getAircraftById(Long id){
-        Optional<Aircraft> aircraftOptional = aircraftRepository.findById(id);
-        return aircraftOptional.orElse(null);
+        return aircraftRepository.findById(id).orElse(null);
     }
 
     public Aircraft createAircraft(Aircraft newAircraft){
         return aircraftRepository.save(newAircraft);
     }
 
-    // Can change this if we establish new arrayList<> in class files instead of leaving it null.
     public Aircraft updateAircraft(Long id, Aircraft updatedAircraft){
         Optional<Aircraft> aircraftOptional = aircraftRepository.findById(id);
-        if (aircraftOptional.isPresent()){
-            Aircraft aircraftToUpdate = aircraftOptional.get();
-            aircraftToUpdate.setType(updatedAircraft.getType());
-            aircraftToUpdate.setAirlineName(updatedAircraft.getAirlineName());
-            aircraftToUpdate.setNumberOfPassengers(updatedAircraft.getNumberOfPassengers());
-            return aircraftRepository.save(aircraftToUpdate);
-        } else {
+        if (aircraftOptional.isEmpty()){
             return null;
         }
+        Aircraft aircraftToUpdate = aircraftOptional.get();
+        aircraftToUpdate.setType(updatedAircraft.getType());
+        aircraftToUpdate.setAirlineName(updatedAircraft.getAirlineName());
+        aircraftToUpdate.setNumberOfPassengers(updatedAircraft.getNumberOfPassengers());
+        return aircraftRepository.save(aircraftToUpdate);
     }
 
     public void deleteAircraftById(Long id){
@@ -57,16 +57,15 @@ public class AircraftService {
         }
     }
 
-    // can also be fixed if we esablish the new arrayList<> in class files
     public void addAirportToAircraft(Long aircraftId, Long airportId){
         Optional<Aircraft> aircraftOptional = aircraftRepository.findById(aircraftId);
         Optional<Airport> airportOptional = airportRepository.findById(airportId);
+
         if (aircraftOptional.isPresent() && airportOptional.isPresent()) {
             Aircraft aircraft = aircraftOptional.get();
             Airport airport = airportOptional.get();
             aircraft.getAirports().add(airport);
             aircraftRepository.save(aircraft);
-            airportRepository.save(airport);
         }
     }
 }
