cd7ed93fdf807ebdc746fe8f9b24b60ce8c3802a
GallantJoseph <gallant.joseph@outlook.com>
2025-11-01 10:51:19 -0230

    Better names for functions

--- src/main/java/rest/aircraft/AircraftController.java
+++ src/main/java/rest/aircraft/AircraftController.java
@@ -17,12 +17,12 @@ public class AircraftController {
     private AircraftService aircraftService;
 
     @GetMapping
-    public ResponseEntity<Iterable<Aircraft>> getAll(){
+    public ResponseEntity<Iterable<Aircraft>> getAllAircraft(){
         return ResponseEntity.ok(aircraftService.getAllAircraft());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Aircraft> getById(@PathVariable Long id){
+    public ResponseEntity<Aircraft> getAircraftById(@PathVariable Long id){
         Aircraft aircraft = aircraftService.getAircraftById(id);
         if (aircraft == null){
             return ResponseEntity.notFound().build();
@@ -31,12 +31,12 @@ public class AircraftController {
     }
 
     @PostMapping
-    public ResponseEntity<Aircraft> create(@RequestBody Aircraft aircraft){
+    public ResponseEntity<Aircraft> createAircraft(@RequestBody Aircraft aircraft){
         return ResponseEntity.ok(aircraftService.createAircraft(aircraft));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Aircraft> update(@PathVariable Long id, @RequestBody Aircraft aircraft){
+    public ResponseEntity<Aircraft> updateAircraft(@PathVariable Long id, @RequestBody Aircraft aircraft){
         Aircraft updated = aircraftService.updateAircraft(id, aircraft);
         if (updated == null){
             return ResponseEntity.notFound().build();
@@ -45,54 +45,49 @@ public class AircraftController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> deleteAircraftById(@PathVariable Long id){
         aircraftService.deleteAircraftById(id);
         return ResponseEntity.noContent().build();
     }
 
-    // Question 2 Aircraft by passenger
     @GetMapping("/passenger/{id}")
-    public ResponseEntity<Iterable<Aircraft>> getByPassenger(@PathVariable Long id){
+    public ResponseEntity<Iterable<Aircraft>> getAllAircraftByPassengerId(@PathVariable Long id){
         return ResponseEntity.ok(aircraftService.getAllAircraftByPassengerId(id));
     }
 
-    // Question 3 Airports by aircraft
     @GetMapping("/{id}/airports")
-    public ResponseEntity<List<Airport>> getAirports(@PathVariable Long id){
-        List<Airport> airport = aircraftService.getAirportsByAircraft(id);
+    public ResponseEntity<List<Airport>> getAirportsByAircraftId(@PathVariable Long id){
+        List<Airport> airport = aircraftService.getAirportsByAircraftId(id);
         if (airport == null){
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(airport);
     }
 
-    // we need this for Question 3
     @PostMapping("/{id}/airports/{airportId}")
-    public ResponseEntity<Void> addAirport(@PathVariable Long id, @PathVariable Long airportId){
+    public ResponseEntity<Void> linkAirportByAircraft(@PathVariable Long id, @PathVariable Long airportId){
         aircraftService.addAirportToAircraft(id, airportId);
         return ResponseEntity.ok().build();
     }
 
-    // Question 2 Aircraft by passenger
     @PostMapping("/{id}/passengers/{passengerId}")
-    public ResponseEntity<Void> addPassenger(@PathVariable Long id, @PathVariable Long passengerId){
+    public ResponseEntity<Void> linkPassengerToAircraft(@PathVariable Long id, @PathVariable Long passengerId){
         aircraftService.addPassengerToAircraft(id, passengerId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}/passengers/{passengerId}")
-    public ResponseEntity<Void> removePassengerFromAircraft(@PathVariable Long id, @PathVariable Long passengerId){
+    public ResponseEntity<Void> unlinkPassengerFromAircraft(@PathVariable Long id, @PathVariable Long passengerId){
         aircraftService.removePassengerFromAircraft(id, passengerId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}/airports/{airportId}")
-    public ResponseEntity<Void> removeAirportFromAircraft(@PathVariable Long id, @PathVariable Long airportId){
+    public ResponseEntity<Void> unlinkAirportFromAircraft(@PathVariable Long id, @PathVariable Long airportId){
         aircraftService.removeAirportFromAircraft(id, airportId);
         return ResponseEntity.ok().build();
     }
 
-    // Question 4
     @GetMapping("/passengers/airports")
     public Set<Airport> getAirportsUsedByAllPassengers() {
         return aircraftService.getAirportsUsedByAllPassengers();
